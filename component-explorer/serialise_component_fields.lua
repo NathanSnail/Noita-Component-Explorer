d3238fb898e3d0dd9f5a75666281649c2cd14d41
Nathan <47441022+NathanSnail@users.noreply.github.com>
2026-08-19 01:40:34 +1200

    fix(serialisation): update the serialisation logic to work with the normalised type names

--- component-explorer/serialise_component_fields.lua
+++ component-explorer/serialise_component_fields.lua
@@ -10,8 +10,7 @@ end
 
 local simple_field_types = as_set({
     "int", "unsignedint", "int16", "uint16", "int32", "uint32", "int64",
-    "uint64", "float", "double", "bool", "std::string", "std_string",
-    "USTRING",
+    "uint64", "float", "double", "bool", "string", "USTRING",
 })
 
 local skip_field_types = as_set({
@@ -42,9 +41,9 @@ function add_field(component_id, component, field_type, field_name)
         return
     elseif field_type == "int" and int_material_field_names[field_name] then
         component.attr[field_name] = CellFactory_GetName(ComponentGetValue2(component_id, field_name))
-    elseif simple_field_types[field_type] or string.find(field_type, "::Enum", 1, true) then
+    elseif simple_field_types[field_type] or string.find(field_type, "_Enum", 1, true) then
         component.attr[field_name] = ComponentGetValue2(component_id, field_name)
-    elseif string.find(field_type, "LensValue<", 1, true) then
+    elseif string.find(field_type, "LensValue_", 1, true) then
         component.attr[field_name] = ComponentGetMetaCustom(component_id, field_name)
     elseif field_type == "StatusEffectType" then
         component.attr[field_name] = ComponentGetMetaCustom(component_id, field_name)
@@ -52,7 +51,7 @@ function add_field(component_id, component, field_type, field_name)
         local x, y = ComponentGetValue2(component_id, field_name)
         component.attr[field_name .. ".x"] = x
         component.attr[field_name .. ".y"] = y
-    elseif field_type == "types::fcolor" then
+    elseif field_type == "types_fcolor" then
         local r, g, b, a = ComponentGetValue2(component_id, field_name)
         component.attr[field_name .. ".r"] = r
         component.attr[field_name .. ".g"] = g
@@ -62,13 +61,13 @@ function add_field(component_id, component, field_type, field_name)
         local min, max = ComponentGetValue2(component_id, field_name)
         component.attr[field_name .. ".min"] = min
         component.attr[field_name .. ".max"] = max
-    elseif field_type == "types::aabb" or field_type == "types::iaabb" then
+    elseif field_type == "types_aabb" or field_type == "types_iaabb" then
         local min_x, min_y, max_x, max_y = ComponentGetValue2(component_id, field_name)
         component.attr[field_name .. ".min_x"] = min_x
         component.attr[field_name .. ".min_y"] = min_y
         component.attr[field_name .. ".max_x"] = max_x
         component.attr[field_name .. ".max_y"] = max_y
-    elseif field_type == "types::xform" then
+    elseif field_type == "types_xform" then
         local x, y, scale_x, scale_y, rotation = ComponentGetValue2(component_id, field_name)
         local transform = nxml.new_element(field_name)
         transform.attr["position.x"] = x
